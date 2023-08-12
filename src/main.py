4100717cf2ef49511ca9b01169a2a60c69c81334
ForwardingAgent <nikolaspdk@gmail.com>
2023-08-12 15:33:27 +0300

    add parametrize

--- src/main.py
+++ src/main.py
@@ -3,9 +3,15 @@ from typing import Union
 
 class Calculator:
     def divide(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arg not numeric")
+        if y == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         return x / y
 
     def add(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arg not numeric")
         return x + y
 
 
